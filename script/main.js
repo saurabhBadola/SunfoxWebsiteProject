d17617d49317f1c999c6545c11f3c704b218669c
suhel hasan <suhelhasan202@gmail.com>
2020-03-03 16:47:57 -0800

    adding new features
    Signed-off-by: suhel hasan <suhelhasan202@gmail.com>

--- script/main.js
+++ script/main.js
@@ -1,7 +1,8 @@
 // changing background color on scroll 
+let selectNav = document.querySelector('.navBar');
 
 window.onscroll = function myFunction() {
-    let selectNav = document.querySelector('.navBar');
+
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60)
         selectNav.classList.add('navBarChanged');
     else
@@ -14,11 +15,21 @@ window.onscroll = function myFunction() {
 
 document.querySelector('.burgerMenu').onclick = function () {
 
-    let displayMe = document.querySelector('.responsiveNavParent');
+    document.querySelector('.responsiveNavParent').classList.toggle('responsiveNavParentChanged');
 
-    if (displayMe.style.display === "none") {
-        displayMe.style.display = "flex";
-    } else {
-        displayMe.style.display = "none";
+
+    document.querySelector('.topSlice').classList.toggle("changeTopSlice");// for animation on
+    document.querySelector('.middleSlice').classList.toggle("changeMiddleSlice");//for removing middleSlice
+    document.querySelector('.bottomSlice').classList.toggle("changeBottomSlice");
+
+
+
+    document.querySelector('.responsiveNav a').onclick = function () {
+        document.querySelector('.responsiveNavParent').classList.toggle('responsiveNavParentChanged');
+
+        document.querySelector('.topSlice').classList.toggle("changeTopSlice");// for animation on
+        document.querySelector('.middleSlice').classList.toggle("changeMiddleSlice");//for removing middleSlice
+        document.querySelector('.bottomSlice').classList.toggle("changeBottomSlice");
     }
+
 }
