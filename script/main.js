df98e082b0e272bdcb05f2903df30782ee0bca55
suhel hasan <suhelhasan202@gmail.com>
2020-03-19 19:05:42 +0530

    adding features
    Signed-off-by: suhel hasan <suhelhasan202@gmail.com>

--- script/main.js
+++ script/main.js
@@ -1,60 +1,37 @@
 // changing background color on scroll 
-let selectNav = document.querySelector('.navBar');
-
-
-
-
 window.onscroll = function myFunction() {
 
     let defaultShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
     function navStyle(color, shadow = defaultShadow) {
+        let selectNav = document.querySelector('.navBar');
+
         selectNav.style.backgroundColor = color;
         selectNav.style.boxShadow = shadow;
     }
 
     if (window.innerHeight / 5 > window.scrollY) {
         navStyle('transparent', 'none');
-
     }
     else if (window.innerHeight > window.scrollY && window.innerHeight / 5 < window.scrollY) {
-
         navStyle('#ef4d58');
-        
     }
     else if (window.innerHeight * 2 > window.scrollY && window.innerHeight < window.scrollY) {
         navStyle('#1a8dff');
-        
-
     }
     else if (window.innerHeight * 3 > window.scrollY && window.innerHeight * 2 < window.scrollY) {
-
         navStyle('#1a8dff');
-        
     }
     else if (window.innerHeight * 4 > window.scrollY && window.innerHeight * 3 < window.scrollY) {
         navStyle('#ef4d58')
-        
     }
     else if (window.innerHeight * 5 > window.scrollY && window.innerHeight * 4 < window.scrollY) {
         navStyle('#1a8dff')
-        
     }
     else if (window.innerHeight * 6 > window.scrollY && window.innerHeight * 5 < window.scrollY) {
         navStyle('#ef4d58');
-        
-
     }
 
-
 }
-
-
-// function staySame() {
-//     if (document.querySelector('.responsiveNavParent').classList.contains("responsiveNavParentChanged")) {
-//         navStyle('transparent', 'none');
-//     }
-// }
-
 
 // carousel in JavaScript
 
@@ -64,8 +41,8 @@ var time = 4000;
 var path = '';
 
 images[0] = "images/bgImage1.png";
-images[1] = "images/image2.jpg";
-images[2] = "images/image3.jpg";
+images[1] = "images/bgImage2.png";
+images[2] = "images/bgImage3.png";
 
 function changeImage() {
 
@@ -77,26 +54,22 @@ function changeImage() {
     else {
         i = 0;
     }
-
     setTimeout('changeImage()', time);
 }
 window.onload = changeImage;
 
-// burger menu
 
+// burger menu for responsive design
 document.querySelector('.burgerMenu').onclick = function () {
-
     changeOnClick();
 
     document.querySelector('.responsiveNav a').onclick = function () {
         changeOnClick();
     }
-
 }
 function changeOnClick() {
     document.querySelector('.responsiveNavParent').classList.toggle('responsiveNavParentChanged');
-    document.querySelector('.topSlice').classList.toggle("changeTopSlice");// for animation on
-    document.querySelector('.middleSlice').classList.toggle("changeMiddleSlice");//for removing middleSlice
+    document.querySelector('.topSlice').classList.toggle("changeTopSlice");
+    document.querySelector('.middleSlice').classList.toggle("changeMiddleSlice");
     document.querySelector('.bottomSlice').classList.toggle("changeBottomSlice");
-
 }
